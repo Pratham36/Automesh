226791fee7fb8adb657da918df4f45e59d936a98
Pratham Ashra <pratham@personal.com>
2026-02-09 18:28:37 +0530

    EditorState

---
+++ src/features/executions/components/http-request/node.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { Node, NodeProps, useReactFlow } from "@xyflow/react"
+import { GlobeIcon } from "lucide-react"
+import { memo, useState } from "react"
+import { BaseExecutionNode } from "../base-execution-node"
+
+type HttpRequestNodeData = {
+    endpoint?: string
+    method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH"
+    body?: string
+    [key: string]: unknown
+}
+
+type HttpRequestNodeType = Node<HttpRequestNodeData>;
+
+export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
+    const nodeData = props.data;
+    const description = nodeData?.endpoint
+        ? `${nodeData.method || "GET"} ${nodeData.endpoint}`
+        : "Not configured"
+
+    return (
+        <>
+            <BaseExecutionNode
+                {...props}
+                icon={GlobeIcon}
+                name="HTTP Request"
+                description={description}
+                onSettings={() => {
+                    // TODO: Implement settings dialog
+                }}
+                onDoubleClick={() => {
+                    // TODO: Implement test execution dialog
+                }}
+            />
+        </>
+    )
+
+});
+
+HttpRequestNode.displayName = "HttpRequestNode";
